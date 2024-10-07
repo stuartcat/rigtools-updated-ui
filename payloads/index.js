0d4256e421059a0f05f3b177ff0de0405fa464ac
htauk <110747219+htauk@users.noreply.github.com>
2024-10-07 17:54:10 -0400

    add adblock button
    add addblock button

--- payloads/index.js
+++ payloads/index.js
@@ -22,6 +22,7 @@ const managementTemplate = `
 <div id="tabs-buttons" style="display: none;">
 <button id="eruda">Load Eruda</button>
 <button id="chii">Load Chii</button>
+<button id="adblock">Adblock</button>
 <button id="ed-hax">Edpuzzle hax</button>
 </div>
 <button id="swamp">Swamp</button>
@@ -679,6 +680,15 @@ const htmlStyle = `
       #ed-hax:hover{
         background-color: #e3b622;
       }
+      #adblock {
+        background-color: #ff4d4d;
+        font-family: Arial;
+        font-size: medium;
+        font-weight: bold;
+      }
+      #adblock:hover {
+        background-color: #ff5d5d;
+      }
       #swamp{
         background-color: #00a5df;
         font-family: Arial;
@@ -862,6 +872,49 @@ onload = async function x() {
       chrome.tabs.executeScript(tabId, { code: chii });
     }
 
+    function runAdblock(tabId) {
+      const adblock = `
+        (function(){
+          /* Ad-B-Gone: Bookmarklet that removes obnoxious ads from pages */
+          var selectors = [
+          /* By ID: */
+          '#sidebar-wrap', '#advert', '#xrail', '#middle-article-advert-container',
+          '#sponsored-recommendations', '#around-the-web', '#sponsored-recommendations',
+          '#taboola-content', '#taboola-below-taboola-native-thumbnails', '#inarticle_wrapper_div',
+          '#rc-row-container', '#ads', '#at-share-dock', '#at4-share', '#at4-follow', '#right-ads-rail',
+    			'div#ad-interstitial', 'div#advert-article', 'div#ac-lre-player-ph',
+    			/* By Class: */
+    			'.ad', '.avert', '.avert__wrapper', '.middle-banner-ad', '.advertisement',
+    			'.GoogleActiveViewClass', '.advert', '.cns-ads-stage', '.teads-inread', '.ad-banner',
+    			'.ad-anchored', '.js_shelf_ads', '.ad-slot', '.antenna', '.xrail-content',
+    			'.advertisement__leaderboard', '.ad-leaderboard', '.trc_rbox_outer', '.ks-recommended',
+    			'.article-da', 'div.sponsored-stories-component', 'div.addthis-smartlayers',
+    			'div.article-adsponsor', 'div.signin-prompt', 'div.article-bumper', 'div.video-placeholder',
+    			'div.top-ad-container', 'div.header-ad', 'div.ad-unit', 'div.demo-block', 'div.OUTBRAIN',
+    			'div.ob-widget', 'div.nwsrm-wrapper', 'div.announcementBar', 'div.partner-resources-block',
+    			'div.arrow-down', 'div.m-ad', 'div.story-interrupt', 'div.taboola-recommended',
+    			'div.ad-cluster-container', 'div.ctx-sidebar', 'div.incognito-modal', '.OUTBRAIN', '.subscribe-button',
+    			'.ads9', '.leaderboards', '.GoogleActiveViewElement', '.mpu-container', '.ad-300x600', '.tf-ad-block',
+    			'.sidebar-ads-holder-top', '.ads-one', '.FullPageModal__scroller',
+    			'.content-ads-holder', '.widget-area', '.social-buttons', '.ac-player-ph',
+    			/* Other: */
+    			'aside#sponsored-recommendations', 'aside[role="banner"]', 'aside',
+    			'amp-ad', 'span[id^=ad_is_]', 'div[class*="indianapolis-optin"]', 'div[id^=google_ads_iframe]',
+    			'div[data-google-query-id]', 'section[data-response]', 'ins.adsbygoogle', 'div[data-google-query-id]',
+    			'div[data-test-id="fullPageSignupModal"]', 'div[data-test-id="giftWrap"]' ];
+    			for(let i in selectors) {
+        			let nodesList = document.querySelectorAll(selectors[i]);
+        			for(let i = 0; i < nodesList.length; i++) {
+            			let el = nodesList[i];
+            			if(el && el.parentNode)
+                	el.parentNode.removeChild(el);
+        			}
+    			}
+			})();
+      `;
+      chrome.tabs.executeScript(tabId, { code: adblock });
+    }
+
     function runEdpuzzle(tabId) {
       const edpuzzle = `
         fetch("https://cdn.jsdelivr.net/gh/Miner49ur/shorthand@main/edpuzzlingscript.js").then(r => r.text()).then(r => {
@@ -895,6 +948,13 @@ onload = async function x() {
       alert("Chii is trying to load [make sure the extension you're running this on is mv2, and has tabs permissions]... \nreload an already loaded website, or load in a new one!");
       listenerApp((tab) => {
         runChii(tab.id);
+      });
+    };
+
+    container_extensions.querySelector("#adblock").onclick = () => {
+      alert("Adblock is trying to load [make sure the extension you're running this on is mv2, and has tabs permissions]... \nreload an already loaded website, or load in a new one!");
+      listenerApp((tab) => {
+        runAdblock(tab.id);
       });
     };
 
