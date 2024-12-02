7b9e05ac6a82a809ddd15aec278522869a5a97e8
stuartcat <118708775+stuartcat@users.noreply.github.com>
2024-12-02 14:00:24 -0800

    Update index.js

--- payloads/index.js
+++ payloads/index.js
@@ -125,10 +125,10 @@ const managementTemplate = `
 <link rel="icon" type="image/x-icon" href="https://raw.githubusercontent.com/T3M1N4L/rigtools-updated-ui/refs/heads/main/docs.ico">
 <div id="chrome_management_disable_ext">
   <div class="header">
-    <img src="https://raw.githubusercontent.com/T3M1N4L/rigtools-updated-ui/refs/heads/main/rigtools-bounce.gif" alt="Rigtools Logo" class="logo" />
+    <img src="https://raw.githubusercontent.com/stuartcat/finnn/refs/heads/main/finn.png" alt="Rigtools Logo" class="logo" />
     <h1> chrome.management Disable Extensions </h1>
   </div>
-  <p class="description">Original repo: https://github.com/T3M1N4L/rigtools-updated-ui/ </p>
+  <p class="description">stuart</p>
   <br />
   <p>Extensions</p>
   <button id="current-extension">Disable injected extension</button>
