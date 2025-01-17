69ef531ceedd1eea9d02ddc9135d4dcb61faee14
stuartcat <118708775+stuartcat@users.noreply.github.com>
2025-01-17 11:26:59 -0800

    Update index.js

--- payloads/index.js
+++ payloads/index.js
@@ -665,7 +665,6 @@ class DefaultExtensionCapabilities {
       <h2>Evaluate code</h1>
         <div class="container">
           <textarea id="code" placeholder="Enter JavaScript to inject"></textarea>
-	  <button id="run-code" onclick="eval(document.getElementById('code').value)>Run</button>
         </div>
        
 
